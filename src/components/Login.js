747b02592900a220c8cf440737a8abbfc177d463
Manolo Alvarez Foro <af.manolo@gmail.com>
2018-05-14 11:15:03 -0400

    routes: Adding Link to home page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-    return(
-        <h1>Login</h1>
+    return (
+        <div>
+            <h1>Login</h1>
+            <Link to="/home"><button>Login</button></Link>
+        </div>
+
     )
 }
 
